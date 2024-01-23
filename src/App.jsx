b47575a9191efdec4133870c85d93cf394567dca
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-01-23 01:14:16 -0500

    torus

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { Canvas } from "@react-three/fiber"
+import { useRef } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import "../src/index.css"
+
+const Torus = () => {
+  const ref = useRef()
+  useFrame((state,delta) => {
+    ref.current.position.z = Math.sin(state.clock.elapsedTime)
+    ref.current.rotation.y = Math.sin(state.clock.elapsedTime)
+  })
+  return (
+    <mesh ref={ref}>
+      <torusGeometry/>
+      <meshStandardMaterial/>
+    </mesh>
+  )
+}
+
 
 function App() {
   return (
     <>
-      <div id="canvas-container">
+      <div id="canvas-container" className=" w-full h-full">
         <Canvas>
-          <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <mesh>
-            <sphereGeometry/>
-            <meshStandardMaterial color="hotpink" />
-          </mesh>
+          <directionalLight intensity={0.5} position={[10,10,10]} />
+          <Torus/>
         </Canvas>
       </div>
     </>
